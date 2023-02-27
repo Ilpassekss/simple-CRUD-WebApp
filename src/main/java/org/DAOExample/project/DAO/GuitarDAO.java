3138255079ea6efd52a5c2b6f960f52666724a0e
p3ntium <ilapaseka27@gmail.com>
2023-02-27 21:28:39 +0100

    add database to my app and wrote code which add info to database

--- src/main/java/org/DAOExample/project/DAO/GuitarDAO.java
+++ src/main/java/org/DAOExample/project/DAO/GuitarDAO.java
@@ -3,33 +3,93 @@ package org.DAOExample.project.DAO;
 import org.DAOExample.project.models.Guitar;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class GuitarDAO {
 
     private List<Guitar> guitars;
+
     private int GUITARS_COUNT ;
-    {
-        guitars = new ArrayList<>();
-        guitars.add(new Guitar(++GUITARS_COUNT, "Gibson LesPaul", 1, "gibson@gmail.com"));
-        guitars.add(new Guitar(++GUITARS_COUNT, "Fender Stratocaster", 40, "fender@mail.com"));
-        guitars.add(new Guitar(++GUITARS_COUNT, "Gibson SG", 5, "gibson@gmail.com"));
-        guitars.add(new Guitar(++GUITARS_COUNT, "Gibson Explorer", 2, "gibson@gmail.com"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/guitars_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "passekss";
+    private static Connection connection;
+
+
+    static {
+        //driver class download and checking
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        //get connection
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
+
+
     public List<Guitar> index(){
-        return guitars;
+
+       List<Guitar> guitarList = new ArrayList<>();
+
+        try {
+            //object in which I collect date from database by url request
+            Statement statement = connection.createStatement();
+
+            //sql code
+            String SQL = "select * from guitar";
+            //statement.executeQuery(SQL); returns Result set object, which collect all data from table
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while(resultSet.next()){
+                Guitar guitar = new Guitar();
+                //get data from resultSet into guitar obj
+                guitar.setId(resultSet.getInt("id"));
+                guitar.setName(resultSet.getString("name"));
+                guitar.setAge(resultSet.getInt("age"));
+                guitar.setDeveloperEmail(resultSet.getString("developer_mail"));
+
+                GUITARS_COUNT = resultSet.getInt("id");
+
+                guitarList.add(guitar);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        }
+        return guitarList;
     }
 
     public Guitar show(int id){
-        return guitars.stream().filter(guitar->guitar.getId() == id ).findAny().orElse(null);
+        //return guitars.stream().filter(guitar->guitar.getId() == id ).findAny().orElse(null);
+        return null;
     }
 
 
     public void save(Guitar guitar) {
-        guitar.setId(++GUITARS_COUNT);
-        guitars.add(guitar);
+       // guitar.setId(++GUITARS_COUNT);
+       // guitars.add(guitar);
+
+        try {
+
+
+            Statement statement = connection.createStatement();
+            int id = ++GUITARS_COUNT;
+
+            String SQL = "insert into Guitar values("+id+",'"+guitar.getName()+"',"+guitar.getAge()+",'"
+                    +guitar.getDeveloperEmail()+"')";
+            //add data to database
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
